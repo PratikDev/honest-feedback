db0ffb7046598516090f9afe23ce71820fd6978a
pratikdev <pratikdevofficial1@gmail.com>
2024-06-23 03:16:28 +0600

    added sign-in page frontend

--- src/app/(frontend)/(auth)/sign-in/page.tsx
+++ src/app/(frontend)/(auth)/sign-in/page.tsx
@@ -1,32 +1,126 @@
 "use client";
 
-import { buttonVariants } from "@/components/ui/button";
-import { signIn, signOut, useSession } from "next-auth/react";
+import { Button } from "@/components/ui/button";
+import {
+	Form,
+	FormControl,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import signInSchema, { signInSchemaType } from "@/schemas/signIn.schema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { signIn } from "next-auth/react";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
+import { toast } from "sonner";
 
 export default function Page() {
-	const { data: session } = useSession();
-	if (session) {
-		return (
-			<div className="p-3">
-				Signed in as {session.user.email} <br />
-				<button
-					className={buttonVariants()}
-					onClick={() => signOut()}
-				>
-					Sign out
-				</button>
-			</div>
-		);
-	}
+	const router = useRouter();
+
+	const form = useForm<signInSchemaType>({
+		resolver: zodResolver(signInSchema),
+		defaultValues: {
+			identifier: "",
+			password: "",
+		},
+	});
+
+	const onSubmit = async (data: signInSchemaType) => {
+		const result = await signIn("credentials", {
+			...data,
+			redirect: false,
+		});
+
+		if (result?.error) {
+			toast.error("Error", {
+				description: result.error,
+			});
+			return;
+		}
+
+		router.push("/dashboard");
+	};
+
 	return (
-		<div className="p-3">
-			Not signed in <br />
-			<button
-				onClick={() => signIn()}
-				className={buttonVariants()}
-			>
-				Sign in
-			</button>
+		<div className="flex justify-center items-center min-h-screen bg-gray-100">
+			<div className="w-full max-w-md p-8 space-y-8 rounded-lg bg-white shadow-md">
+				<div className="text-center">
+					<h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">
+						Join HonestFeedback
+					</h1>
+					<p className="mb-3">
+						Sign in to your account and start sharing your feedback
+					</p>
+				</div>
+
+				<Form {...form}>
+					<form
+						onSubmit={form.handleSubmit(onSubmit)}
+						className="space-y-8"
+					>
+						<div className="space-y-5">
+							<FormField
+								control={form.control}
+								name="identifier"
+								render={({ field }) => (
+									<FormItem>
+										<FormLabel>Username or Email</FormLabel>
+										<FormControl>
+											<Input
+												placeholder="Username or Email"
+												{...field}
+											/>
+										</FormControl>
+										<FormMessage />
+									</FormItem>
+								)}
+							/>
+
+							<FormField
+								control={form.control}
+								name="password"
+								render={({ field }) => (
+									<FormItem>
+										<FormLabel>Password</FormLabel>
+										<FormControl>
+											<Input
+												type="password"
+												placeholder="********"
+												{...field}
+											/>
+										</FormControl>
+										<FormMessage />
+									</FormItem>
+								)}
+							/>
+						</div>
+
+						<Button
+							type="submit"
+							isLoading={form.formState.isSubmitting}
+							className="w-full"
+						>
+							Sign In
+						</Button>
+					</form>
+				</Form>
+
+				<div className="text-center mt-4">
+					<p>
+						Don't have an account yet?{" "}
+						<Link
+							href="/sign-up"
+							className="text-blue-600 hover:underline"
+						>
+							Sign Up
+						</Link>
+					</p>
+				</div>
+			</div>
 		</div>
 	);
 }
